3db2abec6ad1464299f7a3bbc401cf1d4c742ad3
Kyle Gaskill <kylegaskill64@yahoo.com>
2015-12-12 12:17:42 -0500

    Added Main Menu

---
+++ Ludum Dare 34/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	public string levelName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.anyKeyDown) 
+		{
+			Application.LoadLevel(levelName);
+		}
+	}
+}
