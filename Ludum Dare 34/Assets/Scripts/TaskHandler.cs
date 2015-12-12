b9d8458c24c9e1227580c3b85229e66bf0692901
Kyle Gaskill <kylegaskill64@yahoo.com>
2015-12-12 00:20:31 -0500

    Added Task Handler

---
+++ Ludum Dare 34/Assets/Scripts/TaskHandler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaskHandler : MonoBehaviour {
+
+	public int buildLevel = 0;
+	public int buildFinish = 0;
+
+	public bool canFinish = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetButtonDown("Fire1"))
+		{
+			buildLevel += 1;
+			Debug.Log (buildLevel);
+		}
+
+		if (buildLevel == buildFinish) 
+		{
+			canFinish = true;
+		}
+		else 
+		{
+			canFinish = false;
+		}
+
+		if (canFinish)
+		{
+			if(Input.GetButtonDown ("Fire2"))
+			{
+				buildLevel = 0;
+			}
+		}
+
+	}
+}
